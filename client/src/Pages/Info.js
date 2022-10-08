aec2ad4cb06112ca936d946ec06b2de3bdb132e6
HeigenDazs <maxzazaba@gmail.com>
2022-10-08 14:30:55 +0700

    info page

---
+++ client/src/Pages/Info.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link} from "react-router-dom"
+function Info() {
+  return (
+    <div className="App">
+       <Link to="/" > THIS IS THE Info PAGE</Link>
+    </div>
+ 
+  )
+}
+
+export default Info;
