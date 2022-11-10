fb1bfb528d4d801de08a8c6f60bfea9415554cb2
HeigenDazs <maxzazaba@gmail.com>
2022-11-10 21:32:13 +0700

    info page

---
+++ client/src/Pages/InfoData.js
@@ -0,0 +1,272 @@
+export const InfoData = [
+    {
+        id: 1,
+        category: 'Network Fundamentals',
+        display: 'Explain the role and function of network components'        
+    },
+    {
+        id: 2,
+        category: 'Network Fundamentals',
+        display: 'Describe characteristics of network topology architectures'       
+    },
+    {
+        id: 3,
+        category: 'Network Fundamentals',
+        display: 'Compare physical interface and cabling types'       
+    },
+    {
+        id: 4,
+        category: 'Network Fundamentals',
+        display: 'Identify interface and cable issues (collisions, errors, mismatch duplex, and/or speed)'       
+    },
+    {
+        id: 5,
+        category: 'Network Fundamentals',
+        display: 'Compare TCP to UDP'       
+    },
+    {
+        id: 6,
+        category: 'Network Fundamentals',
+        display: 'Configure and verify IPv4 addressing and subnetting'       
+    },
+    {
+        id: 7,
+        category: 'Network Fundamentals',
+        display: 'Describe the need for private IPv4 addressing'       
+    },
+    {
+        id: 8,
+        category: 'Network Fundamentals',
+        display: 'Configure and verify IPv6 addressing and prefix'       
+    },
+    {
+        id: 9,
+        category: 'Network Fundamentals',
+        display: 'Describe IPv6 address types'       
+    },
+    {
+        id: 10,
+        category: 'Network Fundamentals',
+        display: 'Verify IP parameters for Client OS (Windows, Mac OS, Linux)'       
+    },
+    {
+        id: 11,
+        category: 'Network Fundamentals',
+        display: 'Describe wireless principles'       
+    },
+    {
+        id: 12,
+        category: 'Network Fundamentals',
+        display: 'Explain virtualization fundamentals (server virtualization, containers, and VRFs)'       
+    },
+    {
+        id: 13,
+        category: 'Network Fundamentals',
+        display: 'Describe switching concepts'       
+    },
+    
+    {
+        id: 14,
+        category: 'Network Access',
+        display: 'Configure and verify VLANs (normal range) spanning multiple switches'       
+    },
+    {
+        id: 15,
+        category: 'Network Access',
+        display: 'Configure and verify interswitch connectivity'       
+    },
+    {
+        id: 16,
+        category: 'Network Access',
+        display: 'Configure and verify Layer 2 discovery protocols (Cisco Discovery Protocol and LLDP)'       
+    },
+    {
+        id: 17,
+        category: 'Network Access',
+        display: 'Configure and verify (Layer 2/Layer 3) EtherChannel (LACP)'       
+    },
+    {
+        id: 18,
+        category: 'Network Access',
+        display: 'Describe the need for and basic operations of Rapid PVST+ Spanning Tree Protocol and identify basic operations'       
+    },
+    {
+        id: 19,
+        category: 'Network Access',
+        display: 'Compare Cisco Wireless Architectures and AP modes'       
+    },
+    {
+        id: 20,
+        category: 'Network Access',
+        display: 'Describe physical infrastructure connections of WLAN components (AP,WLC, access/trunk ports, and LAG)'       
+    },
+    {
+        id: 21,
+        category: 'Network Access',
+        display: 'Describe AP and WLC management access connections (Telnet, SSH, HTTP,HTTPS, console, and TACACS+/RADIUS)'       
+    },
+    {
+        id: 22,
+        category: 'Network Access',
+        display: 'Configure the components of a wireless LAN access for client connectivity using GUI only such as WLAN creation, security settings, QoS profiles, and advanced WLAN settings'       
+    },
+
+    {
+        id: 23,
+        category: 'IP Connectivity',
+        display: 'Interpret the components of routing table'       
+    },
+    {
+        id: 24,
+        category: 'IP Connectivity',
+        display: 'Determine how a router makes a forwarding decision by default'       
+    },
+    {
+        id: 25,
+        category: 'IP Connectivity',
+        display: 'Configure and verify IPv4 and IPv6 static routing'       
+    },
+    {
+        id: 26,
+        category: 'IP Connectivity',
+        display: 'Configure and verify single area OSPFv2'       
+    },
+    {
+        id: 27,
+        category: 'IP Connectivity',
+        display: 'Describe the purpose of first hop redundancy protocol'       
+    },
+
+    {
+        id: 28,
+        category: 'IP Services',
+        display: 'Configure and verify inside source NAT using static and pools'       
+    },
+    {
+        id: 29,
+        category: 'IP Services',
+        display: 'Configure and verify NTP operating in a client and server mode'       
+    },
+    {
+        id: 30,
+        category: 'IP Services',
+        display: 'Explain the role of DHCP and DNS within the network'       
+    },
+    {
+        id: 31,
+        category: 'IP Services',
+        display: 'Explain the function of SNMP in network operations'       
+    },
+    {
+        id: 32,
+        category: 'IP Services',
+        display: 'Describe the use of syslog features including facilities and levels'       
+    },
+    {
+        id: 33,
+        category: 'IP Services',
+        display: 'Configure and verify DHCP client and relay'       
+    },
+    {
+        id: 34,
+        category: 'IP Services',
+        display: 'Explain the forwarding per-hop behavior (PHB) for QoS such as classification, marking, queuing, congestion, policing, shaping'       
+    },
+    {
+        id: 35,
+        category: 'IP Services',
+        display: 'Configure network devices for remote access using SSH'       
+    },
+    {
+        id: 36,
+        category: 'IP Services',
+        display: 'Describe the capabilities and function of TFTP/FTP in the network'       
+    },
+
+    {
+        id: 37,
+        category: 'Security Fundamentals',
+        display: 'Define key security concepts (threats, vulnerabilities, exploits, and mitigation techniques)'       
+    },
+    {
+        id: 38,
+        category: 'Security Fundamentals',
+        display: 'Describe security program elements (user awareness, training, and physical access control)'       
+    },
+    {
+        id: 39,
+        category: 'Security Fundamentals',
+        display: 'Configure device access control using local passwords'       
+    },
+    {
+        id: 40,
+        category: 'Security Fundamentals',
+        display: 'Describe security password policies elements, such as management, complexity, and password alternatives (multifactor authentication, certificates, and biometrics)'       
+    },
+    {
+        id: 41,
+        category: 'Security Fundamentals',
+        display: 'Describe remote access and site-to-site VPNs'       
+    },
+    {
+        id: 42,
+        category: 'Security Fundamentals',
+        display: 'Configure and verify access control lists'       
+    },
+    {
+        id: 43,
+        category: 'Security Fundamentals',
+        display: 'Configure Layer 2 security features (DHCP snooping, dynamic ARP inspection, and port security)'       
+    },
+    {
+        id: 44,
+        category: 'Security Fundamentals',
+        display: 'Differentiate authentication, authorization, and accounting concepts'       
+    },
+    {
+        id: 45,
+        category: 'Security Fundamentals',
+        display: 'Describe wireless security protocols (WPA, WPA2, and WPA3)'       
+    },
+    {
+        id: 46,
+        category: 'Security Fundamentals',
+        display: 'Configure WLAN using WPA2 PSK using the GUI'       
+    },
+
+    {
+        id: 47,
+        category: 'Automation and Programmability',
+        display: 'Explain how automation impacts network management'       
+    },
+    {
+        id: 48,
+        category: 'Automation and Programmability',
+        display: 'Compare traditional networks with controller-based networking'       
+    },
+    {
+        id: 49,
+        category: 'Automation and Programmability',
+        display: 'Describe controller-based and software defined architectures (overlay, underlay, and fabric)'       
+    },
+    {
+        id: 50,
+        category: 'Automation and Programmability',
+        display: 'Compare traditional campus device management with Cisco DNA Center enabled device management'       
+    },
+    {
+        id: 51,
+        category: 'Automation and Programmability',
+        display: 'Describe characteristics of REST-based APIs (CRUD, HTTP verbs, and data encoding)'       
+    },
+    {
+        id: 52,
+        category: 'Automation and Programmability',
+        display: 'Recognize the capabilities of configuration management mechanisms Puppet, Chef, and Ansible'       
+    },
+    {
+        id: 53,
+        category: 'Automation and Programmability',
+        display: 'Interpret JSON encoded data'       
+    },
+];
