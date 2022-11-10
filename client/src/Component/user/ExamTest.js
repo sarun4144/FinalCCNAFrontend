a685116664befff98149388a6d10679976f1565c
sarun4144 <sarun4144@gmail.com>
2022-11-10 16:08:09 +0700

    sarun

--- client/src/Component/user/ExamTest.js
+++ client/src/Component/user/ExamTest.js
@@ -15,17 +15,6 @@ function ExamTest() {
   const [counter, setCounter] = useState(59);
   const [min, setMin] = useState(59);
 
-   //Question 
-   const [showResults, setShowResults] = useState(false);
-   const [currentQuestion, setCurrentQuestion] = useState(0);
-   const [score, setScore] = useState(0);
-  
-   const Data2 = Data.slice(currentQuestion,currentQuestion+1)
-
-  //Selector
-  const [Selector ,setSelector] = useState(0)
-
-   const restartGame = () => {
   //Question 
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -33,29 +22,12 @@ function ExamTest() {
 
   const Data2 = Data.slice(currentQuestion, currentQuestion + 1)
 
-  const restartGame = () => {
-    setScore(0);
-    setCurrentQuestion(0);
-    setShowResults(false);
-  }
-  function selectCount (isCorrect){
-    setSelector(Selector + 1)
-    optionClicked(isCorrect)
-
-  }
-
-  const optionClicked = (isCorrect) => {
-    // Increment the score
-    // if (isCorrect) {
-    //   setScore(score + 1);
-    // }
-    // if (currentQuestion + 1 < Data.length) {
-    //   setCurrentQuestion(currentQuestion + 1);
-    // } else {
-    //   setShowResults(true);
-    // }
-    console.log("S",isCorrect)
-  }
+  //Selector
+  const [Selector, setSelector] = useState(0)
+  const [ANSCount, setANSCount] = useState(5)
+  const [Block, setBlock] = useState(false)
+  const [selectValue, setselectValue] = useState({})
+  const selectValueS = Object.values(selectValue)
 
   useEffect(() => {
     //code
@@ -68,12 +40,77 @@ function ExamTest() {
     counter >= 0 && setTimeout(() => countdown(), 1000);
   }, [counter]);
 
+  useEffect(() => {
+    console.log('Selector', Selector)
+    if (Selector >= ANSCount) {
+      optionClicked(selectValueS)
+      setBlock(true)
+    }
+  }, [Selector, ANSCount])
+  
+  useEffect(() => {
+    setANSCount(5)
+    setBlock(false)
+    setSelector(0)
+    setselectValue(0)
+  }, [currentQuestion])
+
   function loadData(authtoken) {
     currentexam(authtoken).then((res) => {
       setData(res.data[0].exdata);
     });
   }
 
+  //easyFunction
+  function EasyselectCount(isCorrect, CorrectANS, index) {
+    setselectValue({
+      ...selectValue, [index]: { isCorrect: isCorrect, index: index }
+    })
+    setANSCount(CorrectANS.length)
+    setSelector(Selector + 1)
+
+  }
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    console.log(selectValueS)
+    let i = 0
+    while (i < isCorrect.length) {
+      if (isCorrect[i].isCorrect == false) {
+
+        i = -1
+        break
+      } else {
+        console.log("SSS", isCorrect[i].isCorrect)
+      }
+      // if (currentQuestion + 1 < Data.length) {
+      //   setCurrentQuestion(currentQuestion + 1);
+      // } else {
+      //   setShowResults(true);
+      // }
+
+      i++
+    }
+    if (i == isCorrect.length && i != -1) {
+      setScore(score + 1)
+      console.log("true")
+      if (currentQuestion + 1 < Data.length) {
+        setCurrentQuestion(currentQuestion + 1);
+
+      }
+    } else {
+      console.log("false")
+      if (currentQuestion + 1 < Data.length) {
+        setCurrentQuestion(currentQuestion + 1);
+
+      }
+    }
+  }
+
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  }
   function countdown() {
     if (counter == 0 && min !== 0) {
       setMin(min - 1);
@@ -89,70 +126,86 @@ function ExamTest() {
       setShowResults(true);
     }
   }
-  
+
   if (Type == "Easy") {
-    
+
     return (
       <>
-      {
-        showResults
-        ? (<button onClick={restartGame}>restartGame</button>)
-        :
-      <div className="ExamTcards_wrap">
-        <div className="ExamTcard_item">
-          <div className="ExamTcard_inner">
-            <div className="ExamTrole_name">
-              Easy
-            </div>
-
-            
-            {/* <div className="ExamTQuestion">
-                  <span style={{ fontWeight: "500" }}>Question: {currentQuestion + 1}</span>
-                  <br />
-                  {Data2.map((item , idex) =>
-                    <span>{item.Question}</span>
-                  )}
-                  <br/>
-                  <button onClick={gonext}>GO</button>
-                </div> */}
-            {Data2.map((item, index) => (
+        {
+          showResults
+            ? (
               <div>
-                <img src={item.images.url} />
-                <div className="ExamTQuestion">
-                  <span style={{ fontWeight: "500" }}>Question: {currentQuestion + 1}</span>
-                  <br />
-                  <span>{item.Question}</span>
-                </div>
-                <br />
-                <div className="ExamTtext">
-                  <div className="ExamTChoicepanel">
-                    {item.Choices.map((item, idex) =>
-                      <button className="ExamTButton1" onClick={() => selectCount(item.isCorrect)} >
-                        <div className="ExamTtextarea">
-                          <div className="ExamTnumpanel">
-                            {idex + 1}
-                          </div>
-                          <div className="ExamTtextpanel">
-                            {item.text}
-                          </div>
+                <span>Your Score is {score}</span>
+                <br/>
+                <button onClick={restartGame}>restartGame</button>
+              </div>
+            )
+            :
+            <div className="ExamTcards_wrap">
+              <div className="ExamTcard_item">
+                <div className="ExamTcard_inner">
+                  <div className="ExamTrole_name">
+                    Easy
+                  </div>
+                  {Data2.map((item, index) => (
+                    <div>
+                      <img src={item.images.url} />
+                      <div className="ExamTQuestion">
+                        <span style={{ fontWeight: "500" }}>Question: {currentQuestion + 1}</span>
+                        <br />
+                        <span>{item.Question}</span>
+                        <br />
+                        <br />
+                        <span>{`${Selector}/${item.CorrectANS.length} is Selected `}</span>
+                      </div>
+                      <br />
+                      <div className="ExamTtext">
+                        <div className="ExamTChoicepanel">
+                          {item.Choices.map((item2, idex) =>
+                            <>
+                              {Block
+                                ? (
+                                  <button id={idex + 1} name={idex + 1} className="ExamTButton1" onClick={() => EasyselectCount(item2.isCorrect, item.CorrectANS, idex + 1)} disabled  >
+                                    <div className="ExamTtextarea">
+                                      <div className="ExamTnumpanel">
+                                        {idex + 1}
+                                      </div>
+                                      <div className="ExamTtextpanel">
+                                        {item2.text}
+                                      </div>
+                                    </div>
+                                  </button>
+                                )
+                                : (
+                                  <button id={idex + 1} name={idex + 1} className="ExamTButton1" onClick={() => EasyselectCount(item2.isCorrect, item.CorrectANS, idex + 1)}  >
+                                    <div className="ExamTtextarea">
+                                      <div className="ExamTnumpanel">
+                                        {idex + 1}
+                                      </div>
+                                      <div className="ExamTtextpanel">
+                                        {item2.text}
+                                      </div>
+                                    </div>
+                                  </button>
+                                )
+                              }
+                            </>
+                          )}
                         </div>
-                      </button>
-                    )}
-                  </div>
-                  <br />
+                        <br />
 
-                  <div>
-                    {item.Answerdetail}
-                  </div>
+                        <div>
+                          {item.Answerdetail}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                  <button onClick={gonext}>GO</button>
                 </div>
               </div>
-            ))}
-            <button onClick={gonext}>GO</button>
-          </div>
-        </div>
-      </div>
-  }
-      </>        
+            </div>
+        }
+      </>
     )
   } else {
     if (counter < 0) {
@@ -165,6 +218,6 @@ function ExamTest() {
       );
     }
   }
-}
+
 }
 export default ExamTest;
