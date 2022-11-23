444e3cbcbb069eb77cf202a4bd830631ea425d24
sarun4144 <sarun4144@gmail.com>
2022-11-23 17:20:45 +0700

    sarun

--- client/src/Component/user/Profile.js
+++ client/src/Component/user/Profile.js
@@ -6,27 +6,36 @@ import Swal from 'sweetalert2'
 import LineChart from "./LineChart";
 import RadarChart from "./RadarChart";
 
-import {ChangeName,reads} from "../../Function/Person"
+import { ChangeName, reads ,Hardlog,Easylog} from "../../Function/Person"
 
 function Profile() {
   const user = useSelector((state) => ({ ...state }))
   const Userid = user.userStore.user.ObjectID
   const Token = user.userStore.user.token
   const [data, setData] = useState([]);
+  const [dataExamHard, setDataExamHard] = useState([]);
 
   //console.log(username)
   //console.log(role)
   //console.log(email)
-  console.log(data)
-  useEffect(() =>{
-    loadData(Token,Userid)
-  },[Userid]) 
+  console.log(dataExamHard)
+  useEffect(() => {
+    loadData(Token, Userid)
+  }, [Userid])
+  useEffect(() => {
+    loadExamData(Userid)
+  }, [Userid])
 
-  function loadData(authtoken,id) {
-    reads(authtoken,id).then((res) => {
-        setData(res.data);
+  function loadData(authtoken, id) {
+    reads(authtoken, id).then((res) => {
+      setData(res.data);
     });
-}
+  }
+  function loadExamData(id) {
+    Hardlog(id).then((res) => {
+      setDataExamHard(res.data);
+    });
+  }
 
   const ShowEditUsername = async (id) => {
     const { value: Newusername } = await Swal.fire({
@@ -37,16 +46,16 @@ function Profile() {
       confirmButtonText: 'ยืนยัน',
       confirmButtonColor: 'green',
     })
-    if(Newusername) {
+    if (Newusername) {
       console.log("True")
-      ChangeName(user.userStore.user.token,id,{Newusername})
+      ChangeName(user.userStore.user.token, id, { Newusername })
         .then(res => {
           Swal.fire({
             title: 'แก้ไข Username สำเร็จ',
             confirmButtonText: 'ยืนยัน',
             confirmButtonColor: 'green',
           })
-          loadData(Token,Userid)
+          loadData(Token, Userid)
         }).catch(err => {
           Swal.fire({
             title: err.response.data,
@@ -65,7 +74,7 @@ function Profile() {
           <h1>User - Profile</h1>
         </div>
         <div className="profile-card-content">
-          <div>Username : {data.username} <AiIcons.AiFillEdit id="EditUsernameBtn" onClick={()=>ShowEditUsername(Userid)} /></div>
+          <div>Username : {data.username} <AiIcons.AiFillEdit id="EditUsernameBtn" onClick={() => ShowEditUsername(Userid)} /></div>
           <div>Role : {data.role}</div>
           <div>Email : {data.email}</div>
         </div>
@@ -85,6 +94,23 @@ function Profile() {
             <div className="profile-card-content">
               <RadarChart></RadarChart>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="profile-card">
+        <div className="profile-card-header"><h1>History</h1></div>
+        <div className="row">
+          <div className="col-md-6" style={{textAlign:"center"}}>
+          <h2>Easy</h2>
+          <div className="profile-card-content">
+
+          </div>
+          </div>
+          <div className="col-md-6" style={{textAlign:"center"}}>
+          <h2>Hard</h2>
+          <div className="profile-card-content">
+            
+          </div>
           </div>
         </div>
       </div>
